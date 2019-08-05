a5d903cf149d2f5a7952bea99e47b08cfb256b6d
root <root@shufflehex-new.novalocal>
2019-08-05 11:13:36 +0000

    push from server

--- views/site/index.php
+++ views/site/index.php
@@ -1,14 +1,3 @@
-<?php
-
-/* @var $this yii\web\View */
-
+<?php 
 $this->title = 'Dashboard';
 ?>
-<div class="site-index">
-
-    <div class="jumbotron">
-        <h1>Welcome to Shuffle<span style="color: #EF4836">H</span>ex</h1>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started</a></p>
-    </div>
-</div>
